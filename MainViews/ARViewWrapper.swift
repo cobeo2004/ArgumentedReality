ed2462ce86fd9725baa741434cd854caca0a6e9a
This Is Simon <80842534+cobeo2004@users.noreply.github.com>
2022-03-03 11:36:35 +0700

    Create ARViewWrapper.swift

---
+++ MainViews/ARViewWrapper.swift
@@ -0,0 +1,22 @@
+import RealityKit
+import SwiftUI
+
+public struct ARViewWrapper: UIViewRepresentable {
+    @Binding var selectedShapeIdx : Int;
+    public typealias UIViewType = ARView
+    
+    public func makeCoordinator() -> ARViewCoordinator {
+        return ARViewCoordinator(arViewWrapper: self, selectedShapeIndex: $selectedShapeIdx)
+    }
+    
+    public func makeUIView(context: UIViewRepresentableContext<ARViewWrapper>) -> ARView {
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        arView.enablePlacement()
+        
+        arView.session.delegate = context.coordinator
+        return arView
+    }
+    
+    public func updateUIView(_ uiView: ARView, context: UIViewRepresentableContext<ARViewWrapper>) {
+    }
+}
