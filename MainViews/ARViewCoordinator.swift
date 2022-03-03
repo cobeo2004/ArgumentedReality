c8e4e222d67aae355cf2d89a28e0bdd4ca289012
This Is Simon <80842534+cobeo2004@users.noreply.github.com>
2022-03-03 11:36:05 +0700

    Create ARViewCoordinator.swift

---
+++ MainViews/ARViewCoordinator.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import RealityKit
+import Foundation
+import ARKit
+
+open class ARViewCoordinator : NSObject, ARSessionDelegate {
+    var arViewWrapper: ARViewWrapper
+    @Binding var selectedShapeIdx: Int
+    
+    public init(arViewWrapper wrapper: ARViewWrapper, selectedShapeIndex idx: Binding<Int>) {
+        self.arViewWrapper = wrapper
+        self._selectedShapeIdx = idx
+    }
+}
+
